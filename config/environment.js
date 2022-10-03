c98d48ca2ce11135106c82b93eb53e45afbf32fe
Joe Freeman <joefreeman8@gmail.com>
2022-10-03 11:45:08 +0100

    added note in environment.js

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,3 @@
 export const port = 4000
-export const dbURI = 'mongodb://127.0.0.1/ezis-db'
+export const dbURI = 'mongodb://127.0.0.1/ezis-db' // using 127.0.0.1 because localhost was not working.
 export const secret = 'this is my secret'
