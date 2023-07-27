2ce07577588ae41b24f3bea76f568ec19f998656
Joe Freeman <joefreeman8@gmail.com>
2023-07-27 14:30:20 +0100

    test comment

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,4 @@
 export const port = 4000
 export const dbURI = 'mongodb://127.0.0.1/ezis-db' // using 127.0.0.1 because localhost was not working.
 export const secret = 'this is my secret'
+// test
