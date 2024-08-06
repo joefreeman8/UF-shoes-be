b601978a867806fff916cde15a831a1af5dbf74f
joefreeman8 <joefreeman8@gmail.com>
2024-08-06 12:53:42 +0100

    updated environment variable

--- config/environment.js
+++ config/environment.js
@@ -2,5 +2,5 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const port = process.env.PORT || 4000
-export const dbURI = process.env.DB_URI || 'mongodb://127.0.0.1/ezis-db' // using 127.0.0.1 because localhost was not working.
+export const dbURI = process.env.MONGO_DB_URL || 'mongodb://127.0.0.1/ezis-db' // using 127.0.0.1 because localhost was not working.
 export const secret = process.env.SECRET || 'this is my secret'
