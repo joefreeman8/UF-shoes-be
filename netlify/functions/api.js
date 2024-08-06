67cde7eebea71e192351a5049c1d5212ed1bc4e5
joefreeman8 <joefreeman8@gmail.com>
2024-08-06 12:48:04 +0100

    :

---
+++ netlify/functions/api.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import cors from 'cors'
+import serverless from 'serverless-http'
+
+import logger from '../../lib/logger.js'
+import router from '../../config/router.js'
+import errorHandler from '../../lib/errorHandler.js'
+
+import { connectDb } from '../../db/helpers.js'
+
+
+const app = express()
+
+// Middleware 
+app.use(express.json()) // body parsing logger - enables us to obtain the body from the request
+
+app.use(cors())
+app.use('/', logger) // logging middleware
+app.use('/api', router)
+app.use(errorHandler)
+
+async function startServer() {
+  try {
+    await connectDb()
+    console.log('🤖🚀 Mongo is connected')
+  } catch (err) {
+    console.log('🤖❌ Something went wrong - is your mongo connected?')
+    console.log(err)
+  }
+}
+
+startServer()
+
+export const handler = serverless(app)
